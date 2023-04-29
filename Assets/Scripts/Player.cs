8132f40bd46ad59a155226a7eb0e581d3b89ebdc
EdytheFoster <55165844+EdytheFoster@users.noreply.github.com>
2023-04-29 16:58:46 -0500

    player script code cleanup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,28 +10,26 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //take the current position = new position (0,0,0,)
-        transform.position = new Vector3(0,0,0);
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
+    {
+        CalculateMovement();
+    }
 
 
-    {   float horizontalInput = Input.GetAxis("Horizontal");
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        //move from start position to the right new vector(1, 0, 0) * 0 * real time
-        //move from start position up new vector(0, 1, 0) * 0 * real time
-
         transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * -_speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
 
-        //if player position on y is greater than 0
-        //y position = 0
-        //else if position on y is less than -4.9f 
-        //y position = -4.9f
 
+        //up down
         if (transform.position.y >= 0)
         {
             transform.position = new Vector3(transform.position.x, 0, 0);
@@ -41,11 +39,7 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x, -4.9f, 0);
         }
 
-        //if player position on x is greater than 11.3f
-        //x position = -11.3f
-        //else if position on x is less than -11.3f 
-        //x position = 11.3f
-
+        
         if (transform.position.x >= 11.3f)
         {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
